fdcb68dbbbc30f494aa975735728001d2f5ebb3d
ArthasMil <35243404+ArthasMil@users.noreply.github.com>
2022-08-22 18:20:10 +0800

    Update eval_be_single.py

--- eval_be_single.py
+++ eval_be_single.py
@@ -9,35 +9,10 @@ import numpy as np
 import torch.nn.functional as F
 import scipy.misc
 import cv2
-# import re
 
 torch.backends.cudnn.benchmark = True # this increases inference speed a little
 
-   
-def extract_model_state_dict(ckpt_path, prefixes_to_ignore=[]):
-    checkpoint = torch.load(ckpt_path, map_location=torch.device('cpu'))
-    checkpoint_ = {}
-    if 'state_dict' in checkpoint: # if it's a pytorch-lightning checkpoint
-        for k, v in checkpoint['state_dict'].items():
-            if not k.startswith('model.'):
-                continue
-            k = k[6:] # remove 'model.'
-            for prefix in prefixes_to_ignore:
-                if k.startswith(prefix):
-                    print('ignore', k)
-                    break
-            else:
-                checkpoint_[k] = v
-    else: # if it only has model weights
-        for k, v in checkpoint.items():
-            for prefix in prefixes_to_ignore:
-                if k.startswith(prefix):
-                    print('ignore', k)
-                    break
-            else:
-                checkpoint_[k] = v
-    return checkpoint_
-
+  
 def load_ckpt(model, ckpt_path, prefixes_to_ignore=[]):
     model_dict = model.state_dict()
     checkpoint_ = extract_model_state_dict(ckpt_path, prefixes_to_ignore)
